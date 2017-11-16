49014393709668585f40fbe412ea61435d463cb5
Benjamin Lebris <benjamin.lebris@skillogs.com>
2017-11-16 22:31:59 +0100

    fix useless import

--- src/main/java/com/skillogs/yuza/net/http/UserMapper.java
+++ src/main/java/com/skillogs/yuza/net/http/UserMapper.java
@@ -4,15 +4,8 @@ package com.skillogs.yuza.net.http;
 import com.skillogs.yuza.domain.User;
 import org.mapstruct.Mapper;
 
-import org.mapstruct.factory.Mappers;
-import org.springframework.data.domain.Page;
-
-import java.util.List;
-
-
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
 
     UserDto toDTO(User user);
 
