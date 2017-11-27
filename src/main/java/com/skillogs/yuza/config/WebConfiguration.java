d293184effbb304a8231528f948d7709d7531980
kriyss <kriyssou@gmail.com>
2017-11-27 11:29:44 +0100

    feat: add valve filter on http

--- src/main/java/com/skillogs/yuza/config/WebConfiguration.java
+++ src/main/java/com/skillogs/yuza/config/WebConfiguration.java
@@ -1,6 +1,8 @@
 package com.skillogs.yuza.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.skillogs.yuza.net.http.ValveFilter;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,11 +10,17 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
 
 import static java.util.Arrays.asList;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter implements ServletContextInitializer {
 
     @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
@@ -34,4 +42,13 @@ public class WebConfiguration {
         return new CorsFilter(source);
     }
 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext
+                .addFilter("valveFilter", new ValveFilter())
+                .addMappingForUrlPatterns(
+                        EnumSet.of(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR),
+                        true,
+                        "/*");
+    }
 }
