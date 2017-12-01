6ee9c2e891c9a06112def17bb9d2e87c6dc2696f
kriyss <kriyssou@gmail.com>
2017-12-01 17:10:32 +0100

    fix: correct courses for me

--- src/main/java/com/skillogs/yuza/net/http/user/StudentController.java
+++ src/main/java/com/skillogs/yuza/net/http/user/StudentController.java
@@ -1,10 +1,13 @@
 package com.skillogs.yuza.net.http.user;
 
 import com.skillogs.yuza.domain.Course;
+import com.skillogs.yuza.domain.account.Account;
 import com.skillogs.yuza.domain.user.Classroom;
 import com.skillogs.yuza.repository.ClassroomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,8 +30,18 @@ public class StudentController {
     }
 
     @GetMapping("/{id}/courses")
+    @PreAuthorize("hasAnyAuthority('STUDENT', 'TEACHER', 'ADMIN')")
     public ResponseEntity<Set<Course>> getFollowedCourses(@PathVariable String id) {
         return ResponseEntity.ok(classroomRepository.findByStudentId(id).stream()
+                .map(Classroom::getCourses)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet()));
+    }
+
+    @GetMapping("/me/courses")
+    @PreAuthorize("hasAuthority('STUDENT')")
+    public ResponseEntity<Set<Course>> getFollowedCourses(@AuthenticationPrincipal Account account) {
+        return ResponseEntity.ok(classroomRepository.findByStudentId(account.getId()).stream()
                 .map(Classroom::getCourses)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toSet()));
