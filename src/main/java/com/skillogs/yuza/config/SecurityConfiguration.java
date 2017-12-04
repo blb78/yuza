20744575be70650c2ddd5105c89ff371ea096596
kriyss <kriyssou@gmail.com>
2017-12-04 12:30:27 +0100

    feat: add swagger docs and better valve logging

--- src/main/java/com/skillogs/yuza/config/SecurityConfiguration.java
+++ src/main/java/com/skillogs/yuza/config/SecurityConfiguration.java
@@ -24,7 +24,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/users/authenticate");
+        web.ignoring()
+                .antMatchers("/users/authenticate")
+                .antMatchers("/v2/api-docs/**");
     }
 
     @Override
